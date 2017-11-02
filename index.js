a5c52a8fb4a6a0f9c54807b7c363335c6e79736a
carlosalmonte04 <vacarlos_alm@hotmail.com>
2017-11-02 13:06:24 -0400

    transpile back to es6 for pull request

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-'use strict';
-
-var VueBarcodeScanner = {
-  install: function install(Vue, options) {
+const VueBarcodeScanner = {
+  install (Vue, options) {
     /* global Audio */
     // default plugin setting
-    var attributes = {
+    let attributes = {
       previouseCode: '',
       barcode: '',
       setting: {
@@ -15,127 +13,128 @@ var VueBarcodeScanner = {
       callback: null,
       hasListener: false,
       pressedTime: []
-
-      // initial plugin setting
-    };if (options) {
-      attributes.setting.sound = options.sound || attributes.setting.sound;
-      attributes.setting.soundSrc = options.soundSrc || attributes.setting.soundSrc;
-      attributes.setting.scannerSensitivity = options.sensitivity || attributes.setting.scannerSensitivity;
     }
 
-    Vue.prototype.$barcodeScanner = {};
+    // initial plugin setting
+    if (options) {
+      attributes.setting.sound = options.sound || attributes.setting.sound
+      attributes.setting.soundSrc = options.soundSrc || attributes.setting.soundSrc
+      attributes.setting.scannerSensitivity = options.sensitivity || attributes.setting.scannerSensitivity
+    }
 
-    Vue.prototype.$barcodeScanner.init = function (callback) {
+    Vue.prototype.$barcodeScanner = {}
+
+    Vue.prototype.$barcodeScanner.init = (callback) => {
       // add listenter for scanner
       // use keypress to separate lower/upper case character from scanner
-      addListener('keypress');
+      addListener('keypress')
       // use keydown only to detect Tab event (Tab cannot be detected using keypress)
-      addListener('keydown');
-      attributes.callback = callback;
-    };
+      addListener('keydown')
+      attributes.callback = callback
+    }
 
-    Vue.prototype.$barcodeScanner.destroy = function () {
+    Vue.prototype.$barcodeScanner.destroy = () => {
       // remove listener
-      removeListener('keypress');
-      removeListener('keydown');
-    };
-
-    Vue.prototype.$barcodeScanner.hasListener = function () {
-      return attributes.hasListener;
-    };
-
-    Vue.prototype.$barcodeScanner.getPreviousCode = function () {
-      return attributes.previousCode;
-    };
-
-    Vue.prototype.$barcodeScanner.setSensitivity = function (sensitivity) {
-      attributes.setting.scannerSensitivity = sensitivity;
-    };
-
-    function addListener(type) {
-      if (attributes.hasListener) {
-        removeListener(type);
-      }
-      window.addEventListener(type, onInputScanned);
-      attributes.hasListener = true;
+      removeListener('keypress')
+      removeListener('keydown')
     }
 
-    function removeListener(type) {
+    Vue.prototype.$barcodeScanner.hasListener = () => {
+      return attributes.hasListener
+    }
+
+    Vue.prototype.$barcodeScanner.getPreviousCode = () => {
+       return attributes.previousCode
+    }
+
+    Vue.prototype.$barcodeScanner.setSensitivity = (sensitivity) => {
+      attributes.setting.scannerSensitivity = sensitivity
+    }
+
+    function addListener (type) {
       if (attributes.hasListener) {
-        window.removeEventListener(type, onInputScanned);
-        attributes.hasListener = false;
+        removeListener(type)
+      }
+      window.addEventListener(type, onInputScanned)
+      attributes.hasListener = true
+    }
+
+    function removeListener (type) {
+      if (attributes.hasListener) {
+        window.removeEventListener(type, onInputScanned)
+        attributes.hasListener = false
       }
     }
 
-    function onInputScanned(event) {
+    function onInputScanned (event) {
       // ignore other keydown event that is not a TAB, so there are no duplicate keys
-      if (event.type === 'keydown' && event.keyCode != 9) {
-        return;
+      if (event.type === 'keydown' && event.keyCode != 9 ) {
+        return
       }
 
       if (checkInputElapsedTime(Date.now())) {
         if ((event.keyCode === 13 || event.keyCode === 9) && attributes.barcode !== '') {
           // scanner is done and trigger Enter/Tab then clear barcode and play the sound if it's set as true
-          attributes.callback(attributes.barcode);
+          attributes.callback(attributes.barcode)
           // backup the barcode
-          attributes.previousCode = attributes.barcode;
+          attributes.previousCode = attributes.barcode
           // clear textbox
-          attributes.barcode = '';
+          attributes.barcode = ''
           // clear pressedTime
-          attributes.pressedTime = [];
+          attributes.pressedTime = []
           // trigger sound
           if (attributes.setting.sound) {
-            triggerSound();
+            triggerSound()
           }
           // prevent TAB navigation for scanner
           if (event.keyCode === 9) {
-            event.preventDefault();
+            event.preventDefault()
           }
         } else {
           // scan and validate each charactor
-          attributes.barcode += event.key;
+          attributes.barcode += event.key
         }
       }
     }
 
     // check whether the keystrokes are considered as scanner or human
-    function checkInputElapsedTime(timestamp) {
+    function checkInputElapsedTime (timestamp) {
       // push current timestamp to the register
-      attributes.pressedTime.push(timestamp);
+      attributes.pressedTime.push(timestamp)
       // when register is full (ready to compare)
       if (attributes.pressedTime.length === 2) {
         // compute elapsed time between 2 keystrokes
-        var timeElapsed = attributes.pressedTime[1] - attributes.pressedTime[0];
+        let timeElapsed = attributes.pressedTime[1] - attributes.pressedTime[0];
         // too slow (assume as human)
         if (timeElapsed >= attributes.setting.scannerSensitivity) {
           // put latest key char into barcode
-          attributes.barcode = event.key;
+          attributes.barcode = event.key
           // remove(shift) first timestamp in register
-          attributes.pressedTime.shift();
+          attributes.pressedTime.shift()
           // not fast enough
-          return false;
+          return false
         }
         // fast enough (assume as scanner)
         else {
-            // reset the register
-            attributes.pressedTime = [];
-          }
+          // reset the register
+          attributes.pressedTime = []
+        }
       }
       // not able to check (register is empty before pushing) or assumed as scanner
-      return true;
+      return true
     }
 
     // init audio and play
-    function triggerSound() {
-      var audio = new Audio(attributes.setting.soundSrc);
-      audio.play();
+    function triggerSound () {
+      let audio = new Audio(attributes.setting.soundSrc)
+      audio.play()
     }
   }
-};
+}
 
 // // Automatic installation if Vue has been added to the global scope.
 // if (typeof window !== 'undefined' && window.Vue) {
 //   window.Vue.use(MyPlugin)
 // }
 
-module.exports = VueBarcodeScanner;
+module.exports = VueBarcodeScanner
