dfb9b169c1dd921b28b922273b248a5f89e44b25
BJ Bryant <bj@resaleai.com>
2019-03-08 14:17:34 -0600

    Allow control sequences and non-key13 terminators

--- index.js
+++ index.js
@@ -9,11 +9,33 @@ const VueBarcodeScanner = {
         sound: false,
         soundSrc: '',
         scannerSensitivity: 100,
-        requiredAttr: false
+        requiredAttr: false,
+        // `controlSequenceKeys` should be an array of Strings ([String])
+        // that will be joined in a regex string for identifying
+        // control sequences
+        //
+        // they will be replaced in the return string by tags
+        // example:
+        //   NumLock, 0, 0, 1, 3, NumLock
+        //   is replaced with
+        //   <VControlSequence>0013</VControlSequence>
+        //
+        // this allows easy string replacement
+        controlSequenceKeys: null,
       },
       callback: null,
       hasListener: false,
-      pressedTime: []
+      pressedTime: [],
+      // This is used for scanners which do not send
+      // ENTER (13) as the final key code
+      // in a barcode sequence.
+      timeout: null,
+      // Used to handle control sequences
+      isInControlSequence: false,
+      // Used to emit messages
+      eventBus: null,
+      // Used for determing whether or not to emit a `start` event
+      isProcessing: false,
     }
 
     // initial plugin setting
@@ -22,17 +44,34 @@ const VueBarcodeScanner = {
       attributes.setting.sound = options.sound || attributes.setting.sound
       attributes.setting.soundSrc = options.soundSrc || attributes.setting.soundSrc
       attributes.setting.scannerSensitivity = options.sensitivity || attributes.setting.scannerSensitivity
+      attributes.setting.controlSequenceKeys = options.controlSequenceKeys || null
     }
 
     Vue.prototype.$barcodeScanner = {}
 
-    Vue.prototype.$barcodeScanner.init = (callback) => {
+    Vue.prototype.$barcodeScanner.init = (callback, options) => {
       // add listenter for scanner
       // use keypress to separate lower/upper case character from scanner
       addListener('keypress')
       // use keydown only to detect Tab event (Tab cannot be detected using keypress)
       addListener('keydown')
       attributes.callback = callback
+
+      // allow an event bus to be passed back to the caller
+      //
+      // this is an `init` option because whomever is implementing
+      // this plugin may not want to create additional Vue instances
+      // on every component, but would like to have access to a bus
+      // under some circumstances
+      //
+      // the importance of this is greater when scanning 2D
+      // barcodes, which take significantly longer (>=4 seconds)
+      // than 1D barcodes and some kind of indication of
+      // what the library is doing is useful
+      if (options.eventBus) {
+        attributes.eventBus = new Vue()
+        return attributes.eventBus
+      }
     }
 
     Vue.prototype.$barcodeScanner.destroy = () => {
@@ -46,7 +85,7 @@ const VueBarcodeScanner = {
     }
 
     Vue.prototype.$barcodeScanner.getPreviousCode = () => {
-       return attributes.previousCode
+      return attributes.previousCode
     }
 
     Vue.prototype.$barcodeScanner.setSensitivity = (sensitivity) => {
@@ -68,13 +107,78 @@ const VueBarcodeScanner = {
       }
     }
 
+    // this is called when either an ENTER key (13) is received
+    // or when the `attributes.timeout` fires, following
+    // a scan sequence
+    function finishScanSequence () {
+      // clear and null the timeout
+      if (attributes.timeout) {
+        clearTimeout(attributes.timeout)
+      }
+      attributes.timeout = null
+
+      // scanner is done and trigger Enter/Tab then clear barcode and play the sound if it's set as true
+      attributes.callback(attributes.barcode)
+      // backup the barcode
+      attributes.previousCode = attributes.barcode
+      // clear textbox
+      attributes.barcode = ''
+      // clear pressedTime
+      attributes.pressedTime = []
+      // trigger sound
+      if (attributes.setting.sound) {
+        triggerSound()
+      }
+      emitEvent("finish")
+      attributes.isProcessing = false
+    }
+
+    // if entering a control sequence, add `<VControlSequence>` to the buffer
+    // if exiting a control sequence, add `</VControlSequence>` to the buffer
+    // toggle control sequence flag
+    function handleControlBoundaryKeydown () {
+      attributes.barcode += attributes.isInControlSequence
+        ? "</VControlSequence>"
+        : "<VControlSequence>"
+
+      attributes.isInControlSequence = !attributes.isInControlSequence
+    }
+
+    // Returns a regex or null
+    function controlSequenceRegex () {
+      if (attributes.setting.controlSequenceKeys) {
+        return new RegExp(attributes.setting.controlSequenceKeys.join("|"))
+      }
+      return null
+    }
+
+    function emitEvent (type, payload) {
+      if (attributes.eventBus) {
+        attributes.eventBus.$emit(type, payload)
+      }
+    }
+
     function onInputScanned (event) {
+      const controlRegex = controlSequenceRegex()
+
       // ignore other keydown event that is not a TAB, so there are no duplicate keys
-      if (event.type === 'keydown' && event.keyCode != 9 ) {
-        return
+      if (event.type === 'keydown' && event.keyCode != 9) {
+        // Return early if this is not a control key that should be observed
+        if (controlRegex && !controlRegex.test(event.key)) return
+        // Return early if no control keys should be observed
+        if (!controlRegex) return
+      }
+
+      // handle control boundary keydown
+      if (event.type === 'keydown' && controlRegex && controlRegex.test(event.key)) {
+        return handleControlBoundaryKeydown()
       }
 
       if (checkInputElapsedTime(Date.now())) {
+        if (!attributes.isProcessing) {
+          emitEvent("start", event)
+          attributes.isProcessing = true
+        }
         // check if field has 'data-barcode' attribute
         let barcodeIdentifier = false
         if (attributes.setting.requiredAttr) {
@@ -83,24 +187,20 @@ const VueBarcodeScanner = {
           barcodeIdentifier = true
         }
         if (barcodeIdentifier && (event.keyCode === 13 || event.keyCode === 9) && attributes.barcode !== '') {
-          // scanner is done and trigger Enter/Tab then clear barcode and play the sound if it's set as true
-          attributes.callback(attributes.barcode)
-          // backup the barcode
-          attributes.previousCode = attributes.barcode
-          // clear textbox
-          attributes.barcode = ''
-          // clear pressedTime
-          attributes.pressedTime = []
-          // trigger sound
-          if (attributes.setting.sound) {
-            triggerSound()
-          }
+          finishScanSequence()
+
           // prevent TAB navigation for scanner
           if (event.keyCode === 9) {
             event.preventDefault()
           }
         } else {
-          // scan and validate each charactor
+          // reset the finish sequence timer and add the key to the buffer
+          if (attributes.timeout) {
+            clearTimeout(attributes.timeout)
+          }
+          attributes.timeout = setTimeout(finishScanSequence, attributes.setting.scannerSensitivity)
+
+          // scan and validate each character
           attributes.barcode += event.key
         }
       }
